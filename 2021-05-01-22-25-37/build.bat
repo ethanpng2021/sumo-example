d37edc6616be5fe65df65c5f2a45a5acac5415d0
Middle Tech Guy <77369904+ethanpng2021@users.noreply.github.com>
2021-05-18 10:16:53 +0800

    Deleted unwanted files.

--- 2021-05-01-22-25-37/build.bat
+++
@@ -1,1 +0,0 @@
-python "%SUMO_HOME%\tools\randomTrips.py" -n osm.net.xml --fringe-factor 5 -p 0.390969 -o osm.passenger.trips.xml -e 3600 --vehicle-class passenger --vclass passenger --prefix veh --min-distance 300 --trip-attributes "departLane=\"best\"" --fringe-start-attributes "departSpeed=\"max\"" --allow-fringe.min-length 1000 --lanes --validate
